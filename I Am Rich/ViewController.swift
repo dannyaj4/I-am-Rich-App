fcddd9e92d2930ec8cfd5f006082614dcc0ea27f
Danny A. J <dannyaguswahyudyjeremy@gmail.com>
2020-06-21 16:11:17 +0700

    First Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Danny Aguswahyudy Jeremy on 04/04/20.
+//  Copyright © 2020 Danny Aguswahyudy Jeremy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# I-am-Rich-App
+# I-am-Rich-App
